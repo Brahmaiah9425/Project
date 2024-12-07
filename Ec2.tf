2a177d8080033f3a8d2c557bd9c313ebd2435c03
Brahmaiah.T <bramhithangedupalli@gmail.com>
2024-12-07 14:51:47 +0530

    aa

--- Ec2.tf
+++ Ec2.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "ec2" {
     ami = "ami-0e2c8caa4b6378d8c"
-    instance_type = t2.micro
+    instance_type = "t2.micro"
     key_name = "brahmaiah"
     subnet_id = "subnet-0bba90fb65887ad7d"
   
